41c598960fb35066a9d63e718bb0332d2df6afdd
Dan Moneypenny <catullus1973@gmail.com>
2021-02-07 12:45:08 -0800

    add client base

---
+++ _APP/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+import './index.scss';
+__webpack_nonce__ = process.env.NONCE;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
