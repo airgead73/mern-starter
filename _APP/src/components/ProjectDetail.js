6e48e2d490b14181fb983fd49fa64b8997bf9f23
Dan Moneypenny <catullus1973@gmail.com>
2021-02-07 23:38:19 -0800

    add project detail

--- _APP/src/components/ProjectDetail.js
+++ _APP/src/components/ProjectDetail.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useFetch from './useFetch';
 
 const ProjectDetail = () => {
+
+  const { id } = useParams();
+  const { data: project, error, isPending } = useFetch('http://localhost:4000/projects/' + id)
+
   return ( 
     <React.Fragment>
-      <p>project detail</p>
+      <p>project detail {id}</p>
     </React.Fragment>
    );
 }
