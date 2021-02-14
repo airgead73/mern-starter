a1c4809adb01b0571a4d109281a4248aecb26458
Dan Moneypenny <catullus1973@gmail.com>
2021-02-14 01:25:17 -0800

    nested routing

--- _APP/src/App.js
+++ _APP/src/App.js
@@ -1,33 +1,96 @@
 import React from 'react';
-import { Link, Route, Switch, useRouteMatch } from 'react-router-dom';
+import { Link, Route, Switch, useRouteMatch, useParams } from 'react-router-dom';
 import projects from './data/projects';
+import NotFound from './comps/pages/NotFound';
 
 function Home () {
   return <h1>HOME</h1>
 }
 
-function Project () {
-  return <h2>Project</h2>
+function Task () {
+  const { projectID, taskID } = useParams();
+  console.log(`project: ${projectID}, task: ${taskID}`);
+  const task = projects.find(({ id }) => id === projectID).tasks.find(({ id }) => id === taskID);
+  console.log(task);
+  return <h3>Task {task.title}</h3>
 }
 
-function Projects () {
+function Chapter () {
+  const { projectID, chapterID } = useParams();
+  console.log(`project: ${projectID}, chapter: ${chapterID}`);
+  const chapter = projects.find(({ id }) => id === projectID).chapters.find(({ id }) => id === chapterID);
+  console.log(chapter);
+  return <h3>Chapter {chapter.number}: {chapter.title}</h3>
+}
 
-  const { url, path } = useRouteMatch();
+function Project () {
+
+  const { projectID } = useParams();
+  const { path, url } = useRouteMatch();
+
+  const project = projects.find(({ id }) => id === projectID)
+
+  return (
+
+    <React.Fragment>
+    <article>
+      <h2>{project.code} <small>({project.client})</small></h2>
+      <h3>{project.title}</h3>
+      <p>by {project.author}</p>
+
+      <ul>
+        {project.chapters.map((chapter) => (
+          <li key={chapter.id}>
+            <Link to={`${url}/chapters/${chapter.id}`}>Ch {chapter.number} {chapter.title}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {project.tasks.map((task) => (
+          <li key={task.id}>
+            <Link to={`${url}/tasks/${task.id}`}>{task.title}</Link>
+          </li>
+        ))}
+      </ul>      
+
+    </article>
+
+    <hr/>
+
+    <Route path={`${path}/chapters/:chapterID`}>
+      <Chapter/>
+    </Route>
+    <Route path={`${path}/tasks/:taskID`}>
+      <Task/>
+    </Route>    
+    </React.Fragment>
+
+
+
+  )
+}
+
+function Projects () { 
+
+  const { path, url } = useRouteMatch();
 
   return (
     <React.Fragment>
       <h1>Projects</h1>
       <ul>
-        {projects.map(({ title, code, id }) => (
-          <li key={id}><Link to={`/${url}/${id}`}>{code} {title}</Link></li>
+        {projects.map(({ id, title, code }) => (
+          <li key={id}><Link to={`${url}/${id}`}>{ code } { title }</Link></li>
         ))}
       </ul>
 
       <hr/>
 
-      <Route path={`/${path}/:projectID`}>
+      <Route path={`${path}/:projectID`}>
         <Project/>
       </Route>
+
+
     </React.Fragment>
   )
 }
@@ -35,29 +98,25 @@ function Projects () {
 
 function App() {
   return (
-    <main>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="projects">Projects</Link></li>
-      </ul>
+      <main>
+        <ul>
+          <li><Link to="/" exact="true">Home</Link></li>
+          <li><Link to="/projects">Projects</Link></li>
+        </ul>
 
-    <hr/>  
+        <hr/>  
 
-<Switch>
-                                                        <Route path="/projects">
-                                                              <Projects/>
-                                                            </Route>
-                                                        </Switch>
-    <Route exact path="/">
-      <Home/>
-    </Route> 
+      
+        <Route exact path="/">
+          <Home/>
+          </Route>
+        <Route path="/projects">
+          <Projects/>
+        </Route>
 
-       
+    
+          
     </main>
-
-
-
-
   )
 }
 
--- _APP/src/data/projects.js
+++ _APP/src/data/projects.js
@@ -6,7 +6,33 @@ const projects = [
     edition: 2,
     author: "Taylor, Howard",
     notes: "Notes tbd...",
-    client: "zovio"
+    client: "zovio",
+    chapters: [
+      {
+        id: "chrqtiwe23847t",
+        number: 1,
+        title: "The Sociological Imagination",
+        project: "60202b40057de952a16877f7"
+      },
+      {
+        id: "c7tiituioiiwe2g544eddf",
+        number: 2,
+        title: "Research Methods",
+        project: "60202b40057de952a16877f7"
+      }      
+    ],
+    tasks: [
+      {
+        id: "soc_task1",
+        title: "First task for SOC101",
+        project: "60202b40057de952a16877f7"
+      },
+      {
+        id: "soc_task2",
+        title: "Second task for SOC101",
+        project: "60202b40057de952a16877f7"
+      }      
+    ]
   },{
     id: "60202b6f057de952a16877f8",
     title: "Psychology",
@@ -14,7 +40,33 @@ const projects = [
     edition: 4,
     author: "Rathus, Spencer",
     notes: "Notes tbd...",
-    client: "cengage"
+    client: "cengage",
+    chapters: [
+      {
+        id: "opjwpojfpq093",
+        number: 1,
+        title: "Introduction",
+        project: "60202b6f057de952a16877f8"
+      },
+      {
+        id: "c7tiituioiiwe2g544eddf",
+        number: 2,
+        title: "History & Methods",
+        project: "60202b6f057de952a16877f8"
+      }      
+    ],  
+    tasks: [
+      {
+        id: "psy_task1",
+        title: "First task for PSY101",
+        project: "60202b6f057de952a16877f8"
+      },
+      {
+        id: "psy_task2",
+        title: "Second task for PSY01",
+        project: "60202b6f057de952a16877f8"
+      }      
+    ]      
   },{
     id: "6020b8bf20e7fa45ea30ee00",
     title: "Chemstry",
@@ -22,7 +74,33 @@ const projects = [
     edition: 3,
     author: "Curie, Marie",
     notes: "Notes tbd...",
-    client: "zovio"
+    client: "zovio",
+    chapters: [
+      {
+        id: "opjwpojfpq093",
+        number: 1,
+        title: "History of Chemistry",
+        project: "6020b8bf20e7fa45ea30ee00"
+      },
+      {
+        id: "c7tiituioiiwe2g544eddf",
+        number: 2,
+        title: "Scientific Method",
+        project: "6020b8bf20e7fa45ea30ee00"
+      }      
+    ],
+    tasks: [
+      {
+        id: "chem_task1",
+        title: "First task for CHEM201",
+        project: "6020b8bf20e7fa45ea30ee00"
+      },
+      {
+        id: "chem_task2",
+        title: "Second task for CHEM201",
+        project: "6020b8bf20e7fa45ea30ee00"
+      }      
+    ]        
   }
 ];
 
